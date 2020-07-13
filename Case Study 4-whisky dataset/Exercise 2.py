118f777708ac6470659abb06999803b9fdabed70
Ayooluwa Bamgbade <54252383+debamz@users.noreply.github.com>
2020-07-13 19:11:44 +0100

    Create Exercise 2.py

---
+++ Case Study 4-whisky dataset/Exercise 2.py
@@ -0,0 +1,6 @@
+The following code will complete the exercise:
+
+region_colors = dict(zip(regions, cluster_colors))
+
+print(region_colors)
+{'Speyside': '#0173b2', 'Highlands': '#de8f05', 'Lowlands': '#029e73', 'Islands': '#d55e00', 'Campbelltown': '#cc78bc', 'Islay': '#ca9161'}
