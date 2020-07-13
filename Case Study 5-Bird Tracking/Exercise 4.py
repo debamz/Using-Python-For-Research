c0872080d459e0eee8d415d40345a8561f0ca812
Ayooluwa Bamgbade <54252383+debamz@users.noreply.github.com>
2020-07-13 19:28:02 +0100

    Create Exercise 4.py

---
+++ Case Study 5-Bird Tracking/Exercise 4.py
@@ -0,0 +1,15 @@
+The full solution code is below:
+
+import matplotlib.pyplot as plt
+
+eric_daily_speed  = grouped_birdday.speed_2d.mean()["Eric"]
+sanne_daily_speed = grouped_birdday.speed_2d.mean()["Sanne"]
+nico_daily_speed  = grouped_birdday.speed_2d.mean()["Nico"]
+
+eric_daily_speed.plot(label="Eric")
+sanne_daily_speed.plot(label="Sanne")
+nico_daily_speed.plot(label="Nico")
+plt.legend(loc="upper left")
+plt.show()
+
+nico_daily_speed
