abf5aaa28300e3589b1082cec625d511cdd87e81
Ayooluwa Bamgbade <54252383+debamz@users.noreply.github.com>
2020-07-13 18:57:44 +0100

    Create Exercise 6.py

---
+++ Case Study 2-Hamlet Translations/Exercise 6.py
@@ -0,0 +1,3 @@
+Explanation
+
+You just need to add this line of code to display the plot: plt.show();. Looking at the plot, unique words have the largest difference in statistics by translation - unique English are shorter than either unique Portuguese or unique German words, and there are also fewer unique English words than either unique German or unique Portuguese words.
