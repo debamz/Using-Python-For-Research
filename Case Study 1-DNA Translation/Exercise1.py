5afe8505f60c056ef98f3640a77531fca1f79005
Ayooluwa Bamgbade <54252383+debamz@users.noreply.github.com>
2020-07-13 17:31:35 +0100

    Create Exercise1.py

---
+++ Case Study 1-DNA Translation/Exercise1.py
@@ -0,0 +1,6 @@
+# Let's look at the lowercase letters.
+import string
+string.ascii_lowercase
+
+# We will consider the alphabet to be these letters, along with a space.
+alphabet = " " + string.ascii_lowercase
