9520256be0970d5621aeb8204f1f69cb3da7ea6c
Ayooluwa Bamgbade <54252383+debamz@users.noreply.github.com>
2020-07-16 16:01:56 +0100

    Create Exercise 1.py

---
+++ Case Study 7-2/Exercise 1.py
@@ -0,0 +1,8 @@
+Explanation
+
+These four lines of code will instantiate the regression models and classifiers:
+
+linear_regression = LinearRegression()
+logistic_regression = LogisticRegression()
+forest_regression = RandomForestRegressor(max_depth=4, random_state=0)
+forest_classifier = RandomForestClassifier(max_depth=4, random_state=0)
