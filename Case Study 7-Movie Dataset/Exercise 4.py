ea2609a1b0b05ef4e3b06f8440e73ff3e445df77
Ayooluwa Bamgbade <54252383+debamz@users.noreply.github.com>
2020-07-16 15:50:06 +0100

    Create Exercise 4.py

---
+++ Case Study 7-Movie Dataset/Exercise 4.py
@@ -0,0 +1,14 @@
+The genre indicator columns can be made using the following code:
+
+list_genres = df.genres.apply(lambda x: x.split(","))
+genres = []
+for row in list_genres:
+    row = [genre.strip() for genre in row]
+    for genre in row:
+        if genre not in genres:
+            genres.append(genre)
+
+for genre in genres:
+    df[genre] = df['genres'].str.contains(genre).astype(int)
+
+df[genres].head()
