1f3a1be11d7e7f2709c37ea56343491554b8bbec
Ayooluwa Bamgbade <54252383+debamz@users.noreply.github.com>
2020-07-16 15:19:46 +0100

    Create Exercise 5.py

---
+++ Case Study 6- Homophily of several characteristics of individuals connected in social networks in rural India./Exercise 5.py
@@ -0,0 +1,17 @@
+The full function code is given here:
+
+def homophily(G, chars, IDs):
+    """
+    Given a network G, a dict of characteristics chars for node IDs,
+    and dict of node IDs for each node in the network,
+    find the homophily of the network.
+    """
+    num_same_ties = 0
+    num_ties = 0
+    for n1, n2 in G.edges():
+        if IDs[n1] in chars and IDs[n2] in chars:
+            if G.has_edge(n1, n2):
+                num_ties += 1
+                if chars[IDs[n1]] == chars[IDs[n2]]:
+                    num_same_ties += 1
+    return (num_same_ties / num_ties)
