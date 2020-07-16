61be913c6309bc535ce3025dca4dc96eb0b4a945
Ayooluwa Bamgbade <54252383+debamz@users.noreply.github.com>
2020-07-16 15:14:44 +0100

    Create Exercise 4.py

---
+++ Case Study 6- Homophily of several characteristics of individuals connected in social networks in rural India./Exercise 4.py
@@ -0,0 +1,9 @@
+The following code will give you the chance homophily for each characteristic:
+
+print("Village 1 chance of same sex:", chance_homophily(sex1))
+print("Village 1 chance of same caste:", chance_homophily(caste1))
+print("Village 1 chance of same religion:", chance_homophily(religion1))
+
+print("Village 2 chance of same sex:", chance_homophily(sex2))
+print("Village 2 chance of same caste:", chance_homophily(caste2))
+print("Village 2 chance of same religion:", chance_homophily(religion2))
