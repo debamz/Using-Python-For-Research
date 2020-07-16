3928d460fa1131365826048bb4df0c205f2f93a2
Ayooluwa Bamgbade <54252383+debamz@users.noreply.github.com>
2020-07-16 16:05:30 +0100

    Create Exercise 7.py

---
+++ Case Study 7-2/Exercise 7.py
@@ -0,0 +1,19 @@
+Explanation
+
+The full solution code is given here:
+
+logistic_regression_scores = cross_val_score(logistic_regression, covariates, classification_outcome, cv=10, scoring=accuracy)
+forest_classification_scores = cross_val_score(forest_classifier, covariates, classification_outcome, cv=10, scoring=accuracy)
+
+plt.axes().set_aspect('equal', 'box')
+plt.scatter(logistic_regression_scores, forest_classification_scores)
+plt.plot((0, 1), (0, 1), 'k-')
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.xlabel("Linear Classification Score")
+plt.ylabel("Forest Classification Score")
+
+plt.show();
+
+forest_classifier.fit(covariates, classification_outcome)
+sorted(list(zip(all_covariates, forest_classifier.feature_importances_)), key=lambda tup: tup[1])
